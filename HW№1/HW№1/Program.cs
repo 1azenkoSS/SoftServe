1f3a78684e1b21177342ae31f72c2faeb9e48442
1azenkoSS <140089719+1azenkoSS@users.noreply.github.com>
2023-07-20 23:22:45 +0300

    Add files via upload

---
+++ HW№1/HW№1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace HW_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //First task
+
+            Console.Write("Enter a (side of the square) =  ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Area = {Math.Pow(a,2)}");
+            Console.WriteLine($"Perimeter = {a * 4}");
+
+            //Second task
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            string age = Console.ReadLine();
+            Console.WriteLine($"Okay, your name is {name} and you are {age}");
+
+            //Third task
+
+            double pi = Math.PI;
+            Console.Write("Enter r (radius of a circle) =  ");
+            double r = Convert.ToInt32(Console.ReadLine());
+            double l = 2 * pi * r;
+            double S = pi*Math.Pow(r,2);
+            double volume = 4/3 * pi * Math.Pow(r,3);
+            Console.Write($"The length of the citcle = {l:F2}\n The area of the citcle = {S:F2}\n The volume of the citcle = {volume:F2}\n");
+        }
+    }
+}
