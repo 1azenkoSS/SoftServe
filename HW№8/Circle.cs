b8099463687a6f1aca157ba81292bfab236eae43
1azenkoSS <140089719+1azenkoSS@users.noreply.github.com>
2023-09-03 20:43:22 +0300

    Add files via upload

---
+++ HW№8/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_8
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(string name, double radius) : base(name)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override double Perimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public override void Input()
+        {
+            Console.Write("Enter circle radius: ");
+            Radius = double.Parse(Console.ReadLine());
+        }
+
+        public override void Output()
+        {
+            Console.WriteLine($"Circle: Name = {Name}, Radius = {Radius}, Area = {Area():F2}, Perimeter = {Perimeter():F2}");
+        }
+    }
+}
---
+++ HW№8/Shape.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_8
+{
+    abstract class Shape : IComparable<Shape>
+    {
+        public string Name { get; set; }
+
+        public Shape(string name)
+        {
+            Name = name;
+        }
+
+        public abstract double Area();
+        public abstract double Perimeter();
+
+        public abstract void Input();
+        public abstract void Output();
+
+        public int CompareTo(Shape other)
+        {
+            double thisArea = Area();
+            double otherArea = other.Area();
+
+            if (thisArea < otherArea)
+                return -1;
+            else if (thisArea > otherArea)
+                return 1;
+            else
+                return 0;
+        }
+    }
+}
