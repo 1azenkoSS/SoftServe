966380f32483b3fcdbfd62558788da56b9c93361
1azenkoSS <140089719+1azenkoSS@users.noreply.github.com>
2023-09-18 10:56:00 +0300

    Add files via upload

---
+++ HW#10/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_10
+{
+    public struct Point
+    {
+        public int x, y;
+        public Point(int x, int y)
+        { this.x = x; this.y = y; }
+        public override string ToString() => $"({x},{y})";
+        public double Distance(Point p) => Math.Sqrt(Math.Pow(x - p.x, 2) + Math.Pow(y - p.y, 2));
+    }
+}
